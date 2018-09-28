20f4ba75fb762a817031353f59b41d4fb7606f0c
zwx <zwxrs@sina.com>
2018-09-28 11:01:39 +0800

    firstt_commit

---
+++ a.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 08:15:30 2018
+
+@author: zwxrs
+"""
+
+name = 'Jerry'
+
+def printName(a):
+    print(a)
+    
+num = 3
+
+print(num)
+
+printName(name)
